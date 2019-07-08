681b4e18c2d3acac9586b978de0693a3650790ad
=DamjanRadev <radevdamjan@gmail.com>
2019-07-08 21:41:53 +0200

    Uploading and serving images

--- backend/controllers/feed.js
+++ backend/controllers/feed.js
@@ -22,12 +22,20 @@ exports.createPost = (req, res, next) => {
     error.statusCode = 422;
     throw error; // skips to error handeling middleware
   }
+  if (!req.file) {
+    const error = new Error("No image provided");
+    error.statusCode = 422;
+    throw error;
+  }
+
+  const imageUrl = req.file.path;
   const title = req.body.title;
   const content = req.body.content;
+
   const post = new Post({
     title,
     content,
-    imageUrl: "images/duck.jpg",
+    imageUrl,
     creator: { name: "Damjan" }
   });
   post
