b0807adcdbed3a64557a8f32028b1a195cb63cc5
=DamjanRadev <radevdamjan@gmail.com>
2019-07-08 21:15:34 +0200

    Added error handeling middleware, serving images staticly

--- backend/index.js
+++ backend/index.js
@@ -1,3 +1,5 @@
+const path = require("path");
+
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
@@ -9,6 +11,8 @@ const app = express();
 const feedRoutes = require("./routes/feed");
 
 app.use(bodyParser.json());
+app.use("/images", express.static(path.join(__dirname, "images")));
+
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader(
@@ -18,7 +22,15 @@ app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
   next();
 });
+
 app.use("/feed", feedRoutes);
+
+app.use((error, req, res, next) => {
+  console.log(error);
+  const status = error.statusCode || 500;
+  const message = error.message;
+  res.status(status).json(message);
+});
 
 mongoose
   .connect(MONGODB_URI, { useNewUrlParser: true })
